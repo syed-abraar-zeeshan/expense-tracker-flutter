af7e1f7372863085eab7c907b05c4fee31122e6c
Syed Abraar Zeeshan <abraarzeeshan27@gmail.com>
2026-06-01 19:29:02 +0530

    Added packages in pubspec.yaml

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,9 +3,13 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  connectivity_plus
+  flutter_secure_storage_windows
+  isar_flutter_libs
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
